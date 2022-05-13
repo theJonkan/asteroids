18b06c509189c87955440dcf6d22bd2f3c741db4
jacal732 <jacal732@student.liu.se>
2022-05-13 10:21:42 +0200

    Move private classes inline

--- src/se/liu/jonbj802/collisions/CollisionHandler.java
+++ src/se/liu/jonbj802/collisions/CollisionHandler.java
@@ -33,4 +33,33 @@ public class CollisionHandler
     public void setEnabled(final boolean enabled) {
         this.enabled = enabled;
     }
+
+    /**
+     * Pair allows grouping two CollisionTypes into one type that can be used in maps.
+     * The Pair type is defined like a set, meaning that Pair(a, b) == Pair(b, a).
+     */
+    private class Pair
+    {
+        private final CollisionType collider1;
+        private final CollisionType collider2;
+
+        private Pair(final CollisionType collider1, final CollisionType collider2) {
+            this.collider1 = collider1;
+            this.collider2 = collider2;
+        }
+
+        @Override public boolean equals(final Object obj) {
+            if (this == obj){
+                return true;
+            } if (obj instanceof Pair pair) {
+                return (collider1 == pair.collider1 && collider2 == pair.collider2) || (collider1 == pair.collider2 && collider2 == pair.collider1);
+            }
+
+            return false;
+        }
+
+        @Override public int hashCode() {
+            return collider1.hashCode() + collider2.hashCode();
+        }
+    }
 }
--- src/se/liu/jonbj802/graphics/FileHandler.java
+++ src/se/liu/jonbj802/graphics/FileHandler.java
@@ -77,4 +77,33 @@ public class FileHandler
         cache.put(entry, modified);
         return modified;
     }
+
+    /**
+     * CacheEntry is a small wrapper for putting together an object with name, size and angle.
+     */
+    private class CacheEntry
+    {
+        final String name;
+        final int size, angle;
+
+        private CacheEntry(final String name, final int size, final int angle) {
+            this.name = name;
+            this.size = size;
+            this.angle = angle;
+        }
+
+        @Override public boolean equals(final Object obj) {
+            if (this == obj){
+                return true;
+            } if (obj instanceof CacheEntry cache) {
+                return cache.name.equals(name) && cache.size == size && cache.angle == angle;
+            }
+
+            return false;
+        }
+
+        @Override public int hashCode() {
+            return name.hashCode() + size + angle;
+        }
+    }
 }
